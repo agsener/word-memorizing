8ee7bf9325e1bb42cf1256811af2921fcd9b77e9
agsener <ahmetgokhansener@gmail.com>
2022-11-12 23:55:59 +0300

    first commit

---
+++ src/main/java/com/ags/wordmemorizing/WordMemorizingApplication.java
@@ -0,0 +1,13 @@
+package com.ags.wordmemorizing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordMemorizingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordMemorizingApplication.class, args);
+	}
+
+}
